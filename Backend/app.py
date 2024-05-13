a9c930a81514f4b80409f14e97bbe4e837acc3f8
reztingel <tobias.gamme@gmail.com>
2024-05-13 13:21:21 +0200

    lagt til det som trengs av sqlite3

--- Backend/app.py
+++ Backend/app.py
@@ -1,7 +1,12 @@
 from flask import Flask, render_template, redirect, url_for
+import sqlite3
 
 
 app = Flask(__name__)
+
+
+con = sqlite3.connect("database.db", check_same_thread=False)
+cur = con.cursor()
 
 
 @app.route("/")
