0626d76220bf8dbd794d07072d4bc17ad8f3e2ec
reztingel <tobias.gamme@gmail.com>
2024-05-14 10:49:22 +0200

    lagt til i frontend som puller backend

--- Frontend/app.py
+++ Frontend/app.py
@@ -1,34 +1,26 @@
 from flask import Flask, render_template, request, redirect
 import requests
-
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
+
+
+@app.route("/", methods = ["GET"])
+def index():
+    resturanger = requests.get('http//127.0.0.1:5010/get_resturanger').json()
+    return render_template("index.html", resturanger=resturanger)
+
+@app.route("/get_resturang/<sid>", methods = ["GET"])
+def get_resturang(sid):
+    resturang_meny = requests.get('http://127.0.0.1:5010/get_resturang_meny', json={"sid": sid}).json()
+    resturang = requests.get('http://127.0.0.1:5010/get_resturang', json={"sid": sid}).json()
+    return render_template("resturang.html", resturang=resturang, meny=resturang_meny)
 
 
 
-@app.route('/', methods = ["GET"])
-def entry():
-    return redirect("/front")
-
-@app.route("/front", methods=["GET"])
-def front():
-    url = 'https://fakestoreapi.com/products'
-    response = requests.get(url)
-    data = response.json()
-    return render_template("index.html", data=data)
-
-
-
-@app.route('/get_product/<product_id>', methods = ["GET","POST"])
-def get_product(product_id):
-    if product_id is None:
-        return "No product ID provided"
-    url = 'https://fakestoreapi.com/products/' + product_id
-    response = requests.get(url)
-    data = response.json()
-    return render_template("productt.html", data=data)
 
 
 
 if __name__ =="__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=5000)
