edf6ee1d4feada75ebe83cd63c7e0a8e36b36054
reztingel <tobias.gamme@gmail.com>
2024-05-14 09:46:04 +0200

    lagt til excutes og excutemanys

--- Backend/data.py
+++ Backend/data.py
@@ -38,3 +38,13 @@ meny1 = [{"rid": 1, "rett": "Kjøttboler", "bilde": "kjøttboller.jpg", "beskriv
          {"rid": 1, "rett": "hamburger", "bilde": "burger.jpg", "beskrivelse": "hamburger med ost", "pris": 30},
          {"rid": 1, "rett": "fish n chips", "bilde": "fish_chips.jpg", "beskrivelse": "fish n chips med fish n chips", "pris": 40},
          {"rid": 1, "rett": "pasta", "bilde": "pasta.jpg", "beskrivelse": "pasta med pølse bitter", "pris": 30}]
+
+
+cur.execute("DELETE FROM brukere")
+cur.execute("DELETE FROM resturanger")
+cur.execute("DELETE FROM meny_retter")
+con.commit()
+cur.executemany("INSERT INTO brukere(navn, passord) VALUES(?, 'Passord')", [(bruker,) for bruker in brukere])
+cur.executemany("INSERT INTO resturanger(id,navn) VALUES(?,?)", [(rest["id"], rest["navn"]) for rest in resturanger])
+cur.executemany("INSERT INTO meny_retter(resturang_id, rett, bilde, beskrivelse, pris) VALUES(?,?,?,?,?)", [(rett["rid"], rett["rett"], rett["bilde"], rett["beskrivelse"], rett["pris"]) for rett in meny1])
+con.commit()
