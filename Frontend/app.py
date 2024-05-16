a8b7e432e27a1d42331a4d2e444c4fb877bc3a63
reztingel <tobias.gamme@gmail.com>
2024-05-16 14:01:00 +0200

    fjernet login coder

--- Frontend/app.py
+++ Frontend/app.py
@@ -1,84 +1,19 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 import requests
 from flask_cors import CORS
-from flask_login import LoginManager, login_user, logout_user, login_required, current_user
-from flask_sqlalchemy import SQLAlchemy
-from flask_bcrypt import bcrypt
+from flask.cli import FlaskGroup
+from src import app
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Backend./database.db'
-db = SQLAlchemy()
-login_manager = LoginManager()
-login_manager.init_app(app)
+cli = FlaskGroup(app)
 
 
 app = Flask(__name__)
 CORS(app)
 
-class User(db.Model):
-    """An admin user capable of viewing reports.
 
-    :param str email: email address of user
-    :param str password: encrypted password for the user
-
-    """
-    
-    __tablename__ = 'user'
-
-    email = db.Column(db.String, primary_key=True)
-    passord = db.Column(db.String)
-    authenticated = db.Column(db.Boolean, default=False)
-
-    def is_acitve(self):
-        return True
-    def get_id(self):
-        return self.email
-    def is_authenticated(self):
-        return self.authenticated
-    def is_annonymous(self):
-        return False
-    
-    def load_user(user_id):
-        return User.query.get(user_id)
-
-
-@login_manager.user_loader
-def user_loader(user_id):
-    """Given *user_id*, return the associated User object.
-
-    :param unicode user_id: user_id (email) user to retrieve
-
-    """
-    return User.query.get(user_id)
-
-
-@app.route('/', methods = ["GET", "POST"])
+@app.route('/')
 def index():
-    if current_user.is_authenticated:
-        return redirect(url_for('index'))
-    
-    if request.method == "POST":
-        email = request.form['username']
-        passord = request.form['passord']
-        user = User.query.filter_by(email=email).first()
-        if user and bcrypt.check_password_hash(user.passord, passord):
-            user.authenticated = True
-            db.session.commit()
-            login_user(user)
-            return redirect(url_for('get_resturanger'))
-        else:
-            flash('feil brukernavn eller passord', 'error')
-
-    return render_template("login.html")
-
-@app.route("/logout")
-@login_required
-def logout():
-    current_user.authenticated = False
-    db.session.commit()
-    logout_user()
-    return redirect(url_for('index'))
-
+    return redirect('/get_resturanger')
 
 
 
@@ -99,7 +34,8 @@ def get_resturang(sid):
 
 
 
-
+if __name__ =="__main__":
+    cli()
 
 if __name__ =="__main__":
     app.run(debug=True, port=5000)
