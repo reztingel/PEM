d1ed3a802ea5f72a98b42053ee86d3c757cee921
reztingel <tobias.gamme@gmail.com>
2024-05-16 10:20:25 +0200

    fjernet en error handling

--- Frontend/app.py
+++ Frontend/app.py
@@ -6,29 +6,25 @@ app = Flask(__name__)
 CORS(app)
 
 
-response = requests.get('http://127.0.0.1:5010/get_resturanger')
-
-# Log the response content for debugging
-print("Response content:", response.text)
-
-# Attempt to decode the response as JSON
-try:
-    resturanger = response.json()
-    print("JSON decoded successfully:", resturanger)
-except requests.exceptions.JSONDecodeError as e:
-    print("JSON decode error:", e)
 
 
 @app.route('/', methods = ["GET"])
 def index():
-    resturanger = requests.get('http://127.0.0.1:5010/get_resturanger').json()
+    return redirect('/get_resturanger')
+
+@app.route('/get_resturanger', methods = ["GET"])
+def get_resturanger():
+    resturanger = requests.get('http://127.0.0.1:5001/get_resturanger').json()
     return render_template("index.html", resturanger=resturanger)
 
-@app.route('/get_resturang/<sid>', methods = ["GET"])
+
+
+@app.route('/get_resturang/<int:sid>', methods = ["GET"])
 def get_resturang(sid):
-    resturang_meny = requests.get('http://127.0.0.1:5010/get_resturang_meny', json={"sid": sid}).json()
-    resturang = requests.get('http://127.0.0.1:5010/get_resturang', json={"sid": sid}).json()
+    resturang_meny = requests.get('http://127.0.0.1:5001/get_resturang_meny', json={"sid": sid}).json()
+    resturang = requests.get('http://127.0.0.1:5001/get_resturang', json={"sid": sid}).json()
     return render_template("resturang.html", resturang=resturang, meny=resturang_meny)
+
 
 
 
