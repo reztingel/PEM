08c88d09c3ae77cafcf67620e8b82313eb052d2c
reztingel <tobias.gamme@gmail.com>
2024-05-16 14:01:24 +0200

    fiksa password keyword arguement

--- Frontend/create_user.py
+++ Frontend/create_user.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
 """Create a new admin user."""
-
+import os
 from flask import Flask
 from flask_bcrypt import Bcrypt
 from app import app
 from  users import db, User  # Import your Flask application and User model
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////Backend./database.db'
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'database.db') 
 bcrypt = Bcrypt(app)
 db.init_app(app)
 
@@ -24,10 +25,12 @@ def create_admin_user():
 
         # Prompt for admin user details
         email = input("Enter email address: ")
-        password = bcrypt.generate_password_hash(input("Enter password: "))
+        password = input("Enter password: ")
+        hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
 
         # Create and add the admin user to the database
-        admin_user = User(email=email, password=password, authenticated=True)
+        admin_user = User(email=email, password = hashed_password)
+        admin_user.set_password(password=hashed_password)
         db.session.add(admin_user)
         db.session.commit()
 
