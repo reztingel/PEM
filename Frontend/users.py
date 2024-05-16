7e6b4f7a3c8ed0ab954087a263ee789af5487ae9
reztingel <tobias.gamme@gmail.com>
2024-05-16 14:01:54 +0200

    lagt til mer til passord og login for å fikse error

--- Frontend/users.py
+++ Frontend/users.py
@@ -1,20 +1,47 @@
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+from app import db
 
-db = SQLAlchemy()   
+class User(db.Model):
+    __tablename__ = 'users'
 
-class User(db.Model, UserMixin):
-    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)  # Adjust the length accordingly
 
-    email = db.Column(db.String, primary_key=True)
-    passord = db.Column(db.String)
-    authenticated = db.Column(db.Boolean, default=False)
+    def __init__(self, email, password):
+        self.email = email
+        self.set_password(password)
 
-    def is_acitve(self):
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def is_active(self):
         return True
+
     def get_id(self):
         return self.email
+
     def is_authenticated(self):
-        return self.authenticated
-    def is_annonymous(self):
+        return True
+
+    def is_anonymous(self):
         return False
+
+    @staticmethod
+    def load_user(user_id):
+        return User.query.get(user_id)
+
+
+
+
+
+
+
+    # def set_password(self, password):
+    #     self.password_hash = generate_password_hash(password)
+
+    # def check_password(self, password):
+    #     return check_password_hash(self.password_hash, password)
